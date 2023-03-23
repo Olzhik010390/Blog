07daa35fc0b61b754fbb5b1d8c94cb2dfc850a0b
Olzhik010390 <diko-76@mail.ru>
2023-03-23 18:52:54 +0600

    Сделал сайт со своим блогом.

---
+++ src/App.js
@@ -0,0 +1,22 @@
+
+import './App.css';
+import icon from"./i1.jpg"
+function App() {
+  return (
+    <body>
+      <div className="App">
+        <header>
+          <h1>Welcome to my Blog</h1>
+          <img src={icon}/> 
+        </header>
+        <h2>Hello, today I'm going to tell my life.Let's go!Hello my name is Olzhas.I'm on 12.
+            And I have a little sister, her name is Mellisa.She is on 7.
+            I like ice cream.My favourite type of ice cream is... Chocolate.
+            I born and live in Kazakhstan.And what about you?What can you tell about yourself?
+        </h2>
+      </div>
+    </body>
+  );
+}
+
+export default App;
